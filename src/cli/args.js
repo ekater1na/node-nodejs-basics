74c257d22f3f14aa38233cc69c469c456495e173
ekater1na <ekaterina_l@tut.by>
2022-06-05 22:46:16 +0300

    feat: add cli part

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,11 @@
+import process from "process";
+
 export const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    let value = process.argv[2];
+    let value2 = process.argv[3];
+
+    console.log(`propName is ${value}, prop2Name is ${value2}`);
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,13 @@
+import process from 'process';
+
 export const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const prefix = 'RSS_';
+
+    for (let key in process.env) {
+        console.log(`${prefix}${key}=${process.env[key]}`);
+    }
 };
+
+parseEnv(process.env)
+
